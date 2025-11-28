f1422887e01416dd8914588d053c141c9f9fa1c1
Bhavishya chauhan <bhavishya2468@gmail.com>
2025-11-28 15:25:54 +0530

    changes added

--- server/db/config.js
+++ server/db/config.js
@@ -1,15 +1,14 @@
-const mysql=require("mysql2/promise");
+const mysql = require("mysql2/promise");
 require("dotenv").config();
 
-const con=mysql.createPool({
-    host:process.env.Host,
-    user:process.env.User,
-    password:process.env.Password,
-    database:process.env.Database,
-    waitForConnections:true,
-    connectionLimit:10,
-    queueLimit:0
-})
+const con = mysql.createPool({
+    host: process.env.HOST,
+    user: process.env.USER,
+    password: process.env.PASSWORD,
+    database: process.env.DATABASE,
+    waitForConnections: true,
+    connectionLimit: 10,
+    queueLimit: 0
+});
 
-
-module.exports=con;
+module.exports = con;
